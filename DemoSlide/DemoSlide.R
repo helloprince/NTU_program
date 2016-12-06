0052e2a7413ab2305ab30df64d2df8dbb5dadf39
helloprince <ktiugl@gmail.com>
2016-12-06 15:20:51 +0800

    r

--- DemoSlide/DemoSlide.R
+++ DemoSlide/DemoSlide.R
@@ -1,9 +1,9 @@
 rm(list=ls(all=TRUE))
 library(readr)
-install.packages("devtools")
-require(devtool)
+library(devtool)
 install_github('rCharts', 'ramnathv')
 library(rCharts)
 dataSET <- iris
 names(dataSET) <- gsub("\\.", "", names(dataSET))
 rPlot(SepalLength ~ SepalWidth | Species, data = dataSET, color='Species', type='point')
+
