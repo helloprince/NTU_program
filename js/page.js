d288790909f80cd9ff2ce06817d7085f40152a44
helloprince <ktiugl@gmail.com>
2017-01-07 21:48:59 +0800

    openpage

--- js/page.js
+++ js/page.js
@@ -1,5 +1,5 @@
 function openPage(evt, pageName){
-    var i, x, highLights;
+    var i, x, highLights, map;
     x = document.getElementsByClassName("Page");
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none"; 
@@ -9,6 +9,8 @@ function openPage(evt, pageName){
         highLights[i].className = highLights[i].className.replace("w3-gray", ""); 
         highLights[i].className = highLights[i].className.replace("w3-text-black", "");
     }
+    map = document.getElementById("XMLID_183_");
+    map.openPage(event, 'NTCN');
   document.getElementById(pageName).style.display = "block";
   evt.currentTarget.className += " w3-gray";
   evt.currentTarget.className += " w3-text-black";
